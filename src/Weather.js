f818ba1a95f7c8b2f5b0f3d0a011fef2ac257fa7
Jacky Madenfrost <jacky.madenfrost@gmail.com>
2021-09-28 15:41:01 +1000

    Axios integration

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp} °C`
+    );
+  }
+  let apiKey = "d269e6083d3ccf70ce844f3cbe265377";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Loader
+      type="Puff"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000}
+    />
+  );
+}
